0bf79839b1f2e042280c37dfb76ce3cca40a761f
Christian Heyn <chris.heyn.dd@gmail.com>
2018-07-10 15:16:27 +0200

    prepare str

--- index.hs
+++ index.hs
@@ -1,25 +1,21 @@
 import Data.Char (toLower)
 
+-- Helper
 headToTail (h:str) = str ++ [h]
-toLowerString = map toLower
+toLowerStr = map toLower
+wrap x = x:[]
+prepare str = map wrap $ words str
 
-sameChars :: Int -> String -> String -> Bool
 sameChars i str1 str2 = if (str1 == str2)
     then True
     else if (i >= length str2 || length str1 /= length str2)
         then False
         else sameChars (i + 1) (headToTail str1) str2
 
-areLowerSame s1 s2 = sameChars 0 lowerS1 lowerS2
-    where
-        lowerS1 = toLowerString s1
-        lowerS2 = toLowerString s2
-
-wrap x = x:[]
-
-prepare str = map wrap $ words str
+areLowerSame s1 s2 = sameChars 0 (toLowerStr s1) (toLowerStr s2)
 
 task = do
-     let stringList = prepare "Tokyo London Rome Donlon Kyoto"
-     print(stringList)
--- [["Test"],["test"],["ttt"]]
+    let stringList = prepare "Tokyo London Rome Donlon Kyoto"
+    print(stringList)
+
+-- [["Tokyo"],["London"],["Rome"],["Donlon"],["Kyoto"]]
