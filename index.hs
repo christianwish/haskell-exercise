ba0826ca0fd19934ee3b084dab71fa48f20d906e
Christian Heyn <chris.heyn.dd@gmail.com>
2018-07-10 15:10:55 +0200

    prepare str

--- index.hs
+++ index.hs
@@ -4,11 +4,11 @@ headToTail (h:str) = str ++ [h]
 toLowerString = map toLower
 
 sameChars :: Int -> String -> String -> Bool
-sameChars index str1 str2 = if (str1 == str2)
+sameChars i str1 str2 = if (str1 == str2)
     then True
-    else if (index >= length str2 || length str1 /= length str2)
+    else if (i >= length str2 || length str1 /= length str2)
         then False
-        else sameChars (index + 1) (headToTail str1) str2
+        else sameChars (i + 1) (headToTail str1) str2
 
 areLowerSame s1 s2 = sameChars 0 lowerS1 lowerS2
     where
@@ -19,7 +19,7 @@ wrap x = x:[]
 
 prepare str = map wrap $ words str
 
--- groupWords a =
-
-
+task = do
+     let stringList = prepare "Tokyo London Rome Donlon Kyoto"
+     print(stringList)
 -- [["Test"],["test"],["ttt"]]
