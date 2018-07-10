6ab4e0f318951f948bbd3cf515d8173772990c7f
Christian Heyn <chris.heyn.dd@gmail.com>
2018-07-10 18:17:18 +0200

    getMutationArray

--- index.hs
+++ index.hs
@@ -1,11 +1,11 @@
 import Data.Char (toLower)
-
 -- Helper
 headToTail (h:str) = str ++ [h]
 toLowerStr = map toLower
 wrap x = x:[]
 prepare str = map wrap $ words str
 
+sameChars :: Int -> String -> String -> Bool
 sameChars i str1 str2 = if (str1 == str2)
     then True
     else if (i >= length str2 || length str1 /= length str2)
@@ -13,9 +13,14 @@ sameChars i str1 str2 = if (str1 == str2)
         else sameChars (i + 1) (headToTail str1) str2
 
 areLowerSame s1 s2 = sameChars 0 (toLowerStr s1) (toLowerStr s2)
+firstSame (s1:xs1) (s2:xs2) = areLowerSame s1 s2
+groupItems ls1 ls2 = if (firstSame ls1 ls2) then [ls1 ++ ls2] else [ls1, ls2]
+
+r acc (x:xs) = acc
 
 task = do
     let stringList = prepare "Tokyo London Rome Donlon Kyoto"
-    print(stringList)
+        result = foldl r stringList []
+    print(result)
 
 -- [["Tokyo"],["London"],["Rome"],["Donlon"],["Kyoto"]]
