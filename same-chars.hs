81f0092c049a2a1af07307ec8a13fe071efed0b9
Christian Heyn <chris.heyn.dd@gmail.com>
2018-07-10 18:22:59 +0200

    getMutationArray

--- same-chars.hs
+++ same-chars.hs
@@ -1,9 +1,9 @@
 headToTail str = [(drop 1 str) ++ (take 1 str)]
 
 headToTailRec :: [String] -> Int -> [String]
-headToTailRec acc i = if (i == 0)
-    then acc
-    else headToTailRec (acc ++ (headToTail $ last acc)) (i - 1)
+headToTailRec acc i
+    | (i == 0) = acc
+    | otherwise = headToTailRec (acc ++ (headToTail $ last acc)) (i - 1)
 
 getMutationArray :: String -> [String]
 getMutationArray str = headToTailRec [str] (length str - 1)
