287ce30694ab49c58ba8b1d1aa22b9c7c9ad8774
Christian <chris.heyn.dd@gmail.com>
2018-07-12 13:47:11 +0200

    got it!

--- index.hs
+++ index.hs
@@ -1,3 +1,4 @@
+import Data.List (zipWith)
 import Data.Char (toLower)
 
 toLowerStr = map toLower
@@ -17,8 +18,17 @@ mutationList str = headToTail `imap` (cloneList str)
 areSame :: String -> String -> Bool
 areSame s1 s2 = toLowerStr s1 `elem` (mutationList $ toLowerStr s2)
 
-innerSame :: [String] -> [String] -> Bool
-innerSame (x:xs) (y:ys) = areSame x y
+areNotSame :: String -> String -> Bool
+areNotSame s1 s2 = not $ areSame s1 s2
 
--- [["Tokyo"],["London"],["Rome"],["Donlon"],["Kyoto"]]
--- [["Tokyo"] ["Kyoto"]]
+groupValues :: [String] -> [[String]]
+groupValues (x:xs) = a:[] ++ (groupValues b)
+    where
+    a = x:(filter (areSame x) xs)
+    b = filter (areNotSame x) xs
+groupValues _ = []
+
+-- groupValues ["Tokyo", "London", "Rome", "Donlon", "Kyoto"]
+-- >>> [["Tokyo","Kyoto"],["London","Donlon"],["Rome"]]
+
+
