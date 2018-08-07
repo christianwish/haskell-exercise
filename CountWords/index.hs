42cd8528e68ea92d6b128fa53828cc658d1672ad
Christian Heyn <chris.heyn.dd@gmail.com>
2018-08-07 11:09:47 +0200

    task improvement

--- CountWords/index.hs
+++ CountWords/index.hs
@@ -1,10 +1,11 @@
 import Data.Char (toLower)
 import System.IO
 
+-- Helper
 toLowerString = map toLower
 _NON_WORDS = ['.', ',', '?', '!', '"', '(', ')']
-
-replaceNonWords = map (\x -> if x `elem` _NON_WORDS then ' ' else x)
+replaceChar x = if x `elem` _NON_WORDS then ' ' else x
+replaceNonWords str = map replaceChar (toLowerString str)
 
 uniqWords :: [String] -> String -> [String]
 uniqWords [] w = w:[]
@@ -12,21 +13,27 @@ uniqWords acc w
     | w `elem` acc = acc
     | otherwise = w:[] ++ acc
 
-getWords :: String -> [String]
-getWords str = foldl uniqWords [] w
-    where w = map toLowerString (words clearedStr)
+getUniqWords :: String -> [String]
+getUniqWords str = foldl uniqWords [] w
+    where w = words clearedStr
           clearedStr = replaceNonWords str
 
-countWords :: String -> String -> Int
-countWords blacklist input = length restWords
+countWords :: String -> String -> (Int, Int)
+countWords blacklist input = (a, length restInputWords)
     where
-        restWords = filter (\x -> x `notElem` uniqBlacklist) uniqInput
-        uniqInput = getWords input
-        uniqBlacklist = getWords blacklist
+        a = length restUniqeWords
+        b = length restInputWords
+        restUniqeWords = filter (`notElem` uniqBlacklist) uniqInput
+        restInputWords = filter (`notElem` uniqBlacklist) allInputWords
+        uniqInput = getUniqWords input
+        allInputWords = words $ replaceNonWords input
+        uniqBlacklist = getUniqWords blacklist
 
 main = do
     putStrLn "Eingabe:\n"
     userInput <- getLine
     handle <- openFile "./blacklist.txt" ReadMode
     fileContent <- hGetContents handle
-    print $ countWords fileContent userInput
+    let result = countWords fileContent userInput
+        s = (show $ snd result) ++ " Woerter, davon " ++ (show $ fst result) ++ " verschieden."
+    print s
